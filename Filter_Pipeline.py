736aa6d33af31969fbc261c56e72e35a6e2ffdea
kylec43 <kylecurry43@gmail.com>
2021-02-28 02:11:49 -0600

    commit

--- Filter_Pipeline.py
+++ Filter_Pipeline.py
@@ -3,7 +3,7 @@ def Filter_Pipeline(data, *filters):
 	transformed_data = data
 
 	for filter in filters:
-		filter.Add_Data(transformed_data)
+		filter.Process_Data(transformed_data)
 		transformed_data = filter.Get_Transformed_Data()
 
 	return transformed_data
