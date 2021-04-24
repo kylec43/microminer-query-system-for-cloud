804127427de2efd22181115f1d94ff4bddd74b6b
kylec43 <kylecurry43@gmail.com>
2021-04-24 04:34:46 -0500

    commit

--- Constants.py
+++ Constants.py
@@ -29,7 +29,7 @@ ARG_URL = u'url'
 ARG_NOISE_WORDS = u'noiseWords'
 ARG_KEYWORDS = u'keywords'
 
-DATABASE_URL = 'https://database-manager-dot-kwic-project.uc.r.appspot.com'
+DATABASE_URL = 'https://kwic-project.uc.r.appspot.com'
 KWIC_WEBSERVER_URl = 'https://kwic-webserver-dot-kwic-project.uc.r.appspot.com'
 
 
