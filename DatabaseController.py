edad5324036d4d19e54f757de15bf0dfcbed2c91
kylec43 <kylecurry43@gmail.com>
2021-04-24 02:48:57 -0500

    commit

--- DatabaseController.py
+++ DatabaseController.py
@@ -13,7 +13,8 @@ class DatabaseController:
 		try:
 			#Recieve a response from the Server
 			response = requests.get(Constants.DATABASE_URL, params={Constants.GET_ARG_NOISE_WORDS: noiseWords, Constants.GET_ARG_ORIGINAL_URL_KEYWORDS: originalUrlKeywords, Constants.GET_ARG_KWIC_URL_KEYWORDS: kwicUrlKeywords, Constants.GET_ARG_REQUEST_TYPE: Constants.REQUEST_TYPE_UPLOAD}, timeout = 60)
-			if response.text == Constants.SERVER_RESPONSE_UPLOAD_FAILURE:
+			response = response.text
+			if response == Constants.SERVER_RESPONSE_UPLOAD_FAILURE:
 				raise Exception(Constants.SERVER_RESPONSE_UPLOAD_FAILURE)
 
 		except Exception as e:
@@ -32,6 +33,7 @@ class DatabaseController:
 		error = None
 		try:	
 			queryResults = requests.get(Constants.DATABASE_URL, params={Constants.GET_ARG_KEYWORDS: keywords, Constants.GET_ARG_REQUEST_TYPE: Constants.REQUEST_TYPE_QUERY}, timeout=60)
+			queryResults = queryResults.text
 			if queryResults == Constants.SERVER_RESPONSE_QUERY_FAILURE:
 				raise Exception('SEARCH FAILURE')
 
